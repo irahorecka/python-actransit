281329aa295f47d4d5e1e68f5ff6ea636f979632
Ira Horecka <ira89@icloud.com>
2020-01-19 10:36:19 -0800

    upload package to pypi

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## python-ac-transit
 
-A simple Alameda County Transit wrapper
+A simple Alameda-Contra Costa Transit District API wrapper.
---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Setup for the ac_transit package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Ira Horecka",
+    author_email="ira89@icloud.com",
+    name='ac_transit',
+    license="MIT",
+    description='ac_transit is a python package for querying data from the AC Transit API.',
+    version='v0.0.1',
+    long_description=README,
+    url='https://github.com/irahorecka/python-ac-transit',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['requests', 'gtfs-realtime-bindings', 'protobuf3-to-dict'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
