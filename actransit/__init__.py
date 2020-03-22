3830ee7000dd82b930a1a75855a7f2668ec94ba8
Ira Horecka <ira89@icloud.com>
2020-03-22 14:35:29 -0700

    modify doc in __init__.py

--- actransit/__init__.py
+++ actransit/__init__.py
@@ -1,4 +1,4 @@
-"""ac_transit is a python package for querying API
+"""python-actransit is a python package for querying API
 data from Alameda-Contra Costa Transit District Transit."""
 
 from . import actransit
