f36422509930883a124680e3d943c29fe13dc926
Ira Horecka <ira89@icloud.com>
2025-03-16 19:18:27 -0400

    update version from 0.1.4 --> 0.1.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     name="python-actransit",
     license="MIT",
     description="Simple Alameda-Contra Costa Transit District (AC Transit) API wrapper",
-    version="v0.1.4",
+    version="v0.1.5",
     long_description=README,
     url="https://github.com/irahorecka/python-actransit",
     packages=["actransit"],
